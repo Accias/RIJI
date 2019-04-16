df8da524f3368848406cfa66b48a5f39ca0bf730
Sarina <sarinaxi2001@gmail.com>
2019-04-16 13:41:33 -0400

    day UI interface

---
+++ app/src/main/java/com/example/riji/TableofYear.java
@@ -0,0 +1,14 @@
+package com.example.riji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableofYear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tableof_year);
+    }
+}
